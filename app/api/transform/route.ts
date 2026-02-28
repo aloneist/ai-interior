a1fa859b66c56ac92f5261755083b92f46cd9c44
aloneist <callkang05@gmail.com>
2026-02-28 06:56:44 +0000

    transformlast

--- app/api/transform/route.ts
+++ app/api/transform/route.ts
@@ -1,7 +1,7 @@
 import Replicate from "replicate";
 
 const replicate = new Replicate({
-  auth: process.env.REPLICATE_API_TOKEN!,
+  auth: process.env.REPLICATE_API_TOKEN,
 });
 
 export async function POST(req: Request) {
